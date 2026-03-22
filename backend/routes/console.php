638fafd1d7cb60ec1a4eedbcdab0a0958d9942b2
Albert Developer <albert.developer@yandex.ru>
2026-03-23 02:52:03 +0300

    Добавил описание новой ветки. А также добавил команду для ручного прогрева кеша

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Services\SlotService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
@@ -7,5 +8,10 @@ use Illuminate\Support\Facades\Schedule;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('redis:warmup', function (SlotService $service) {
+    $service->warmUpCache();
+    $this->info('Redis cache has been warmed up!');
+})->purpose('Fill Redis ZSET and HASH from MySQL');
 
 Schedule::command('holds:clear-expired')->everyMinute();
